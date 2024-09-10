a304b296763c381dffd9c63faaa31914c90560b2
Jin Nan Goto <43223423+jgoto@users.noreply.github.com>
2024-09-10 18:15:40 -0400

    Added leaflet js maps to the earthquake data cards

--- src/maps.js
+++ src/maps.js
@@ -1,11 +1,18 @@
-function createLocationMap(id, coordinates){
+const createLocationMap = ((id, coordinates)=>{
     const locationMap = document.createElement("div");
     locationMap.id=id;
     locationMap.classList.add("map");
     const map = L.map(locationMap).setView([coordinates[1],coordinates[0]],13);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{
-        maxZoom: 19,
+        maxZoom: 6,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+    const circle = L.circle([coordinates[1], coordinates[0]], {
+        color: 'red',
+        fillColor: "#f03",
+        fillOpacity: 0.5,
+        radius: 30000
+    }).addTo(map);
+    setTimeout(function(){map.invalidateSize()},200);
     return locationMap;
-}
+})
