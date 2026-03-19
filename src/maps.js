651f33234e4d8f4da4b9f98ba30949bf3bcd2267
Jin Nan Goto <43223423+jgoto@users.noreply.github.com>
2026-03-18 20:16:47 -0400

    Updated map image provider to USGS.USTopo instead of openstreetmaps

--- src/maps.js
+++ src/maps.js
@@ -1,17 +1,19 @@
+const MAP_SOURCE = 'https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{x}/{y}'
+
 const createLocationMap = ((id, coordinates)=>{
     const locationMap = document.createElement("div");
     locationMap.id=id;
     locationMap.classList.add("map");
-    const map = L.map(locationMap).setView([coordinates[1],coordinates[0]],7);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{
-        maxZoom: 11,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    const map = L.map(locationMap).setView([coordinates[1],coordinates[0]],5);
+    L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}',{
+        maxZoom: 13,
+        attribution: 'Tiles courtesy of the <a href="https://usgs.gov">U.S. Geological Survey</a>',
     }).addTo(map);
     const circle = L.circle([coordinates[1], coordinates[0]], {
         color: 'red',
         fillColor: "#f03",
         fillOpacity: 0.5,
-        radius: 30000
+        radius: 90000
     }).addTo(map);
     setTimeout(function(){map.invalidateSize()},200);
     return locationMap;
